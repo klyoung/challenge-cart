8d901c7e4c305b9b2c45a2085bc8f0840598087b
Kristoffer Tonning <tonning@gmail.com>
2022-04-12 14:34:32 +0200

    Add empty tests

---
+++ tests/Unit/CartTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CartTest extends TestCase
+{
+    public function test_that_it_can_add_an_item_to_the_cart()
+    {
+        //
+    }
+
+    public function test_that_it_can_remove_an_item_from_the_cart()
+    {
+        //
+    }
+
+    public function test_that_it_can_increase_the_quantity_of_an_item_in_the_cart()
+    {
+        //
+    }
+
+    public function test_that_it_can_decrease_the_quantity_of_an_item_in_the_cart()
+    {
+        //
+    }
+
+    public function test_that_it_can_add_a_discount_to_the_cart()
+    {
+        //
+    }
+
+    public function test_that_it_can_remove_a_discount_to_the_cart()
+    {
+        //
+    }
+
+    public function test_that_it_can_get_the_subtotal_of_the_cart_without_a_discount()
+    {
+        //
+    }
+
+    public function test_that_it_can_get_the_subtotal_of_the_cart_with_a_discount()
+    {
+        //
+    }
+
+    public function test_that_it_can_get_the_total_of_the_cart_without_a_discount()
+    {
+        //
+    }
+
+    public function test_that_it_can_get_the_total_of_the_cart_with_a_discount()
+    {
+        //
+    }
+}
